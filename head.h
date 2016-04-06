b7fa1369eed6a71aebcac866ff1849a19007a7ed
gerodimos <kokatrio@gmail.com>
2016-04-06 20:19:08 +0300

    all implementation on header

--- head.h
+++ head.h
@@ -15,3 +15,20 @@ public:
     void hello();
 
 };
+
+
+template <class T>
+Point<T>::Point(){
+    x = rand() % 20 - 10;
+    y = rand() % 20 - 10;
+}
+
+template <class T>
+Point<T>::Point(T a,T b){
+    x=a,y=b;
+}
+
+template <class T>
+void Point<T>::hello(){
+    std::cout<<"hello there"<<std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -6,21 +6,6 @@
 using namespace std;
 
 
-template <class T>
-Point<T>::Point(){
-    x = rand() % 20 - 10;
-    y = rand() % 20 - 10;
-}
-
-template <class T>
-Point<T>::Point(T a,T b){
-    x=a,y=b;
-}
-
-template <class T>
-void Point<T>::hello(){
-    cout<<"hello there"<<endl;
-}
 
 
 
